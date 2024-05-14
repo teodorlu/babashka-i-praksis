141744c624e9d32d938d96746b9d802d5b03b756
Teodor Heggelund <git@teod.eu>
2024-05-14 11:44:07 +0200

    the-scripts

--- src/live/how_process.clj
+++ src/live/how_process.clj
@@ -1,2 +1,6 @@
-(ns live.how-process)
+(ns live.how-process
+  (:require
+   [babashka.process :as process]
+   [clojure.string :as str]))
 
+(str/split-lines (:out (process/shell {:out :string} "ls")))
---
+++ what.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+open -a firefox slides/1101-babashka-what-why.pdf
